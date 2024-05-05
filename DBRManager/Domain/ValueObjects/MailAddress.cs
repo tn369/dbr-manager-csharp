8be8f76bdf41a92a3ef0cb92e95c46fd63b79357
nakatomi taichi <nakatomi369@gmail.com>
2024-05-06 07:39:57 +0900

    update MailAddressのバリデーションを追加

--- DBRManager/Domain/ValueObjects/MailAddress.cs
+++ DBRManager/Domain/ValueObjects/MailAddress.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Domain.ValueObjects
 {
-    internal sealed record MailAddress
+    internal sealed partial record MailAddress
     {
         public MailAddress(string value)
         {
@@ -14,10 +15,32 @@ namespace Domain.ValueObjects
             {
                 throw new ArgumentException($"MailAddress を null または空白にすることはできません。", nameof(value));
             }
+            var regex = truePattern();
+
+            if (!regex.IsMatch(value))
+            {
+                throw new ArgumentException($"MailAddress の形式が誤っています。", nameof(value));
+            }
+
+            foreach (var item in value.Split("@"))
+            {
+                if (item.StartsWith('.'))
+                {
+                    throw new ArgumentException($"MailAddress の形式が誤っています。", nameof(value));
+                }
+
+                if (item.EndsWith('.'))
+                {
+                    throw new ArgumentException($"MailAddress の形式が誤っています。", nameof(value));
+                }
+            }
 
             Value = value;
         }
 
         public string Value { get; }
+
+        [GeneratedRegex(@"^[a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~.]+@[a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~.]+$")]
+        private static partial Regex truePattern();
     }
 }
--- DBRManager/DomainTest/ValueObjectTests/MailAddressTests.cs
+++ DBRManager/DomainTest/ValueObjectTests/MailAddressTests.cs
@@ -23,5 +23,69 @@ namespace DomainTest.ValueObjectTests
             var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
             Assert.Contains("MailAddress を null または空白にすることはできません。", exception.Message);
         }
+
+        [Theory]
+        [InlineData ("aieuo1234gmail.com")]
+        [InlineData ("^-0987654321gmail.com")]
+        public void アットマークがない場合エラー(string value)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress の形式が誤っています。", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("@")]
+        [InlineData("@gmail.com")]
+        [InlineData("@g")]
+        [InlineData("hogehoge@")]
+        [InlineData("e@")]
+        public void アットマークの前後に文字がない場合エラー(string value)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress の形式が誤っています。", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("test@じーめーる.com")]
+        [InlineData("test@ｇｍａｉｌ．ｃｏｍ")]
+        [InlineData("ｔｅｓｔ@gmail.com")]
+        public void 全角文字が含まれる場合エラー(string value)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress の形式が誤っています。", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("test@test@gmail.com")]
+        [InlineData("test@@gmail.com")]
+        [InlineData("test@@@@gmail.com")]
+        public void アットマークが複数ある場合エラー(string value)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress の形式が誤っています。", exception.Message);
+        }
+
+
+        [Theory]
+        [InlineData("test@gmail.com.")]
+        [InlineData(".test@gmail.com")]
+        [InlineData("test.@gmail.com")]
+        [InlineData("test@.gmail.com")]
+        public void 先頭末尾にドットがある場合エラー(string value)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress の形式が誤っています。", exception.Message);
+        }
+
+        // RFC規定上はNGだが携帯キャリアメールの一部で許容されるケースがあるため除外
+        // https://ja.wikipedia.org/wiki/%E3%83%A1%E3%83%BC%E3%83%AB%E3%82%A2%E3%83%89%E3%83%AC%E3%82%B9
+        //[Theory]
+        //[InlineData("test@gmail..com.")]
+        //[InlineData("test..test@gmail.com")]
+        //public void ドットが二連続している場合エラー(string value)
+        //{
+        //    var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+        //    Assert.Contains("MailAddress の形式が誤っています。", exception.Message);
+        //}
     }
 }
