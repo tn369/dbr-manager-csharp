a02e3d633a011b1bb8e245a2ed23c9310e3ccd0c
nakatomi taichi <nakatomi369@gmail.com>
2024-05-06 07:27:43 +0900

    add MailAddressValueObject

---
+++ DBRManager/Domain/ValueObjects/MailAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    internal sealed record MailAddress
+    {
+        public MailAddress(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException($"MailAddress を null または空白にすることはできません。", nameof(value));
+            }
+
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}
---
+++ DBRManager/DomainTest/ValueObjectTests/MailAddressTests.cs
@@ -0,0 +1,27 @@
+﻿using Domain.ValueObjects;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace DomainTest.ValueObjectTests
+{
+    public class MailAddressTests
+    {
+        [Fact]
+        public void NULLの場合エラー()
+        {
+            string value = null;
+
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress を null または空白にすることはできません。", exception.Message);
+        }
+
+        [Fact]
+        public void スペースの場合エラー()
+        {
+            string value = "   ";
+
+            var exception = Assert.Throws<ArgumentException>(() => new MailAddress(value));
+            Assert.Contains("MailAddress を null または空白にすることはできません。", exception.Message);
+        }
+    }
+}
