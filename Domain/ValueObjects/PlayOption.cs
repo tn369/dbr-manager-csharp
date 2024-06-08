52fa965b7172db583ff283cc761c975f6df2789b
nakatomi taichi <nakatomi369@gmail.com>
2024-06-09 00:00:08 +0900

    fix typo

--- Domain/ValueObjects/PlayOption.cs
+++ Domain/ValueObjects/PlayOption.cs
@@ -1,4 +1,26 @@
-﻿namespace Domain.ValueObjects
+﻿using Domain.Enums;
+
+namespace Domain.ValueObjects
 {
-    public record PlayOption(bool AutoScratch, bool LegacyNote, bool Flip);
+    public sealed record PlayOption(bool AutoScratch, bool LegacyNote, bool Flip)
+    {
+        public PlayOption WithAutoScratch(bool autoScratch) => this with { AutoScratch = autoScratch };
+        public PlayOption WithLegacyNote(bool legacyNote) => this with { LegacyNote = legacyNote };
+        public PlayOption WithFlip(bool flip) => this with { Flip = flip };
+
+        public static PlayOption Create(GameModeStatus mode, bool autoScratch, bool legacyNote, bool flip)
+        {
+            if (mode.Mode == GameMode.Battle)
+            {
+                return new PlayOption(autoScratch, legacyNote, Flip: false);
+            }
+
+            if (mode.Mode == GameMode.Single)
+            {
+                return new PlayOption(autoScratch, legacyNote, Flip: false);
+            }
+
+            return new PlayOption(autoScratch, legacyNote, flip);
+        }
+    }
 }
