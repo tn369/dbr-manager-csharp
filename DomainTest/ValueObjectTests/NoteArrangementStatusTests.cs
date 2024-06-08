262c53c2bde9141d8fee3a647f727e2ebd558b49
nakatomi taichi <nakatomi369@gmail.com>
2024-06-08 23:52:40 +0900

    test add NoteArrangementStatus Tests

---
+++ DomainTest/ValueObjectTests/NoteArrangementStatusTests.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Enums;
+using Domain.ValueObjects;
+using Xunit;
+namespace DomainTest.ValueObjectTests
+{
+    namespace DomainTests
+    {
+        public class NoteArrangementStatusTests
+        {
+            [Theory]
+            [InlineData(NoteArrangement.Normal, "Normal")]
+            [InlineData(NoteArrangement.Random, "Random")]
+            [InlineData(NoteArrangement.RRandom, "R-Random")]
+            [InlineData(NoteArrangement.SRandom, "S-Random")]
+            [InlineData(NoteArrangement.HRandom, "H-Random")]
+            [InlineData(NoteArrangement.Mirror, "Mirror")]
+            public void GetName_ShouldReturnCorrectName(NoteArrangement arrangement, string expectedName)
+            {
+                // Arrange
+                var noteArrangementStatus = new NoteArrangementStatus(arrangement);
+
+                // Act
+                var actualName = noteArrangementStatus.GetName();
+
+                // Assert
+                Assert.Equal(expectedName, actualName);
+            }
+
+            [Fact]
+            public void GetName_ShouldThrowArgumentOutOfRangeException_ForUnknownNoteArrangement()
+            {
+                // Arrange
+                var invalidArrangement = (NoteArrangement)999;
+                var noteArrangementStatus = new NoteArrangementStatus(invalidArrangement);
+
+                // Act & Assert
+                Assert.Throws<ArgumentOutOfRangeException>(() => noteArrangementStatus.GetName());
+            }
+        }
+    }
+}
