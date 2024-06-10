3f9150a280b26ad7431036094e5eea13cfb29528
nakatomi taichi <nakatomi369@gmail.com>
2024-06-10 21:13:29 +0900

    Remove infrastructure code from domain layer

--- Domain/Entities/Player.cs
+++ Domain/Entities/Player.cs
@@ -2,15 +2,11 @@
 
 namespace Domain.Entities
 {
-    public sealed class Player(PlayerId playerId, IidxId iidxId, Email email, PlayerName name, Url url, EncryptPassword encryptPassword, Salt salt)
+    public sealed class Player(PlayerId playerId, IidxId iidxId, PlayerName name, Url url)
     {
         public PlayerId PlayerId { get; private set; } = playerId;
         public IidxId IidxId { get; private set; } = iidxId;
-        public Email Email { get; private set; } = email;
         public PlayerName Name { get; private set; } = name;
         public Url Url { get; private set; } = url;
-        public EncryptPassword EncryptPassword { get; private set; } = encryptPassword;
-        public Salt Salt { get; private set; } = salt;
     }
-
 }
--- DomainTest/EntytyTests/PlayerTests.cs
+++ DomainTest/EntytyTests/PlayerTests.cs
@@ -12,21 +12,15 @@
         {
             var playerId = new PlayerId(1);
             var iidxId = new IidxId("1234-5678");
-            var email = new Email("test@example.com");
             var name = new PlayerName("PlayerName");
             var url = new Url("http://example.com");
-            var encryptPassword = new EncryptPassword("password");
-            var salt = new Salt("salt");
 
-            var player = new Player(playerId, iidxId, email, name, url, encryptPassword, salt);
+            var player = new Player(playerId, iidxId, name, url);
 
             Assert.Equal(playerId, player.PlayerId);
             Assert.Equal(iidxId, player.IidxId);
-            Assert.Equal(email, player.Email);
             Assert.Equal(name, player.Name);
             Assert.Equal(url, player.Url);
-            Assert.Equal(encryptPassword, player.EncryptPassword);
-            Assert.Equal(salt, player.Salt);
         }
     }
 }
