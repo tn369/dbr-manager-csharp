0a74c3d336a796c396a134c747e7f2b078a6bd5c
nakatomi <36021243+tn369@users.noreply.github.com>
2025-06-06 22:40:09 +0900

    Fix PlayerScoreRepository interface and implement methods

--- src/Domain/Repositories/IPlayerScoreRepository.cs
+++ src/Domain/Repositories/IPlayerScoreRepository.cs
@@ -5,7 +5,7 @@ namespace Domain.Repositories
 {
     public interface IPlayerScoreRepository : IRepository<PlayerScore, (PlayerId, MusicId, Difficulty, PlayAt)>
     {
-        Task<IEnumerable<PlayerScore>> GetScoresByPlayerIdAsync(Guid playerId);
-        Task<IEnumerable<PlayerScore>> GetScoresByMusicIdAsync(Guid musicId);
+        Task<IEnumerable<PlayerScore>> GetScoresByPlayerIdAsync(PlayerId playerId);
+        Task<IEnumerable<PlayerScore>> GetScoresByMusicIdAsync(MusicId musicId);
     }
 }
