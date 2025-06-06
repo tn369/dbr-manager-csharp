134a82ef0e226af880c046cd5cc99f77ba1f429c
nakatomi <36021243+tn369@users.noreply.github.com>
2025-06-07 05:53:31 +0900

    Add composite key configuration for PlayerScore

--- src/Infrastructure/EFCore/ApplicationDbContext.cs
+++ src/Infrastructure/EFCore/ApplicationDbContext.cs
@@ -11,5 +11,18 @@ namespace Infrastructure.Repositories
         public DbSet<Player> Players { get; set; }
         public DbSet<PlayerScore> PlayerScores { get; set; }
         public DbSet<GameVersion> GameVersions { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PlayerScore>()
+                .HasKey(ps => new { ps.PlayerId, ps.MusicId, ps.Difficulty, ps.PlayAt });
+        }
     }
 }
