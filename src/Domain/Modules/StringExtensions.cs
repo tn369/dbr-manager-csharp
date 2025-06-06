efbad9f302c631b06dc3eba1109ea68940235515
nakatomi <36021243+tn369@users.noreply.github.com>
2025-06-06 23:07:53 +0900

    fix: adjust StringExtensions style

--- src/Domain/Modules/StringExtensions.cs
+++ src/Domain/Modules/StringExtensions.cs
@@ -2,14 +2,14 @@
 
 namespace Domain.Modules
 {
-    static internal partial class StringExtensions
+    internal static partial class StringExtensions
     {
         /// <summary>
         /// 全角数字を半角数字に変換する。
         /// </summary>
         /// <param name="value">変換する文字列</param>
         /// <returns>変換後の文字列</returns>
-        static public string ConvertFullToHalfNumbers(this string value)
+        public static string ConvertFullToHalfNumbers(this string value)
         {
             return FullNumbers().Replace(value, p => ((char)(p.Value[0] - '０' + '0')).ToString());
         }
